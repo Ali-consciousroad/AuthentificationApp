c6a6ed5d369ad7d8340359542aacccc2168cb3a6
Ali-consciousroad <ali_dindar@live.be>
2021-12-03 23:17:58 +0100

    Building & using a custom context provider component

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},
+    // We currently do nothing with those data but they would be needed in a real app
+    onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // Executed by React after component reevaluation 
+    /* Thanks to useEffect(), we execute this anonymous function only once 
+    otherwise we would have gotten an infinite loop  
+    */
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+        if (storedUserLoggedInInformation === "1") {
+        setIsLoggedIn(true);
+        }
+    }, []);
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        localStorage.setItem("isLoggedIn", "1");
+        setIsLoggedIn(true);
+    };
+
+    return (
+        <AuthContext.Provider
+          value={{
+            isLoggedIn: isLoggedIn,
+            onLogout: logoutHandler,
+            onLogin: loginHandler,
+          }}
+        >
+          {props.children}
+        </AuthContext.Provider>);
+};
 
 export default AuthContext;
