40e7d16f3aff69ea1f54a2f505573d86843f65c4
Ali-consciousroad <ali_dindar@live.be>
2021-12-06 16:57:32 +0100

    Diving into Forward Refs allowing parent comp. to use child comp. functionalities

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, { useState, useEffect, useReducer, useContext, useRef } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -48,6 +48,9 @@ const Login = (props) => {
   });
 
   const authCtx = useContext(AuthContext); 
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -101,13 +104,20 @@ const Login = (props) => {
 
     const submitHandler = (event) => {
       event.preventDefault();
-      authCtx.onLogin(emailState.value, passwordState.value);
+      if (formIsValid) {
+        authCtx.onLogin(emailState.value, passwordState.value);
+      } else if (!emailIsValid) {
+        emailInputRef.current.focus();
+      } else {
+        passwordInputRef.current.focus();
+      }
     };
 
     return (
       <Card className={classes.login}>
         <form onSubmit={submitHandler}>
           <Input
+            ref={emailInputRef}
             id="email"
             label="E-mail"
             type="email"
@@ -117,6 +127,7 @@ const Login = (props) => {
             onBlur={validateEmailHandler}
           />
            <Input
+            ref={passwordInputRef}
             id="password"
             label="Password"
             type="password"
@@ -129,7 +140,6 @@ const Login = (props) => {
             <Button
               type="submit"
               className={classes.btn}
-              disabled={!formIsValid}
             >
               Login
             </Button>
